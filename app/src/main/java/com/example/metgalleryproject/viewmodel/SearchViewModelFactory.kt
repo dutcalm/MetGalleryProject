c758a0ba1278ede12dcf53ae9f1d2a7b1deb34a8
dutcalm <dutca.lucia.marcelina@gmail.com>
2025-05-13 22:21:19 +0300

    + Jetpack paging

--- app/src/main/java/com/example/metgalleryproject/viewmodel/SearchViewModelFactory.kt
+++ app/src/main/java/com/example/metgalleryproject/viewmodel/SearchViewModelFactory.kt
@@ -10,7 +10,7 @@ class SearchViewModelFactory(
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(SearchViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return SearchViewModel(repository) as T
+            return SearchViewModel() as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
